2dd9b9b7f9de5bb9939ab975d40a95c9422a8b56
younesbardach <85373688+YounesBardach@users.noreply.github.com>
2024-11-18 12:20:19 -0500

    Functions name changes
    Changed names of functions to explain what they do

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const testArrayOne = [-8, 2, 2, 3, 9];
 const testArrayTwo = [-3, -2, -1, 0];
 const testArrayThree = [-8, 8, -6, 1, 3, -9, 3, 2, 1];
 
-const twoWayMergeSort = (arrayLeft, arrayRight) => {
+const mergeSortedArrays = (arrayLeft, arrayRight) => {
   let sortedArray = [];
   while (arrayLeft.length && arrayRight.length) {
     if (arrayLeft[0] < arrayRight[0]) {
@@ -36,14 +36,14 @@ const twoWayMergeSort = (arrayLeft, arrayRight) => {
 };
 // console.log(twoWayMergeSort(testArrayOne, testArrayTwo));
 
-const mergeSort = (array) => {
+const mergeSortAlgo = (array) => {
   if (array.length < 2) {
     return array;
   }
   const mid = array.length / 2;
   const rightArray = array.splice(0, mid);
 
-  return twoWayMergeSort(mergeSort(array), mergeSort(rightArray));
+  return mergeSortedArrays(mergeSortAlgo(array), mergeSortAlgo(rightArray));
 };
 
 // console.log(mergeSort(testArrayThree));
